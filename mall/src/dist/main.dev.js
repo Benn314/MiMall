b9b6fab695a96aa7791bcc6221d700c26109cd4f
Benn314 <209441806@qq.com>
2022-09-21 08:43:35 +0800

    项目目录结构

--- mall/src/dist/main.dev.js
+++ mall/src/dist/main.dev.js
@@ -1,4 +1,9 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// createApp(App).mount('#app')
 "use strict";
+
+var _vue = require("vue");
+
+var _App = _interopRequireDefault(require("./App.vue"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _vue.createApp)(_App["default"]).mount('#app');
