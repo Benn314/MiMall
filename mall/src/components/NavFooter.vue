55ee31df4dc4120a309e3c0b011e66d3daa5be33
Benn314 <209441806@qq.com>
2022-09-27 21:08:51 +0800

    Navfooter组件开发

--- mall/src/components/NavFooter.vue
+++ mall/src/components/NavFooter.vue
@@ -5,13 +5,13 @@
             <p>小米商城</p>
         </div>
         <div class="footer-link">
-            <a href="">河畔一角主页</a> <span>|</span>
-            <a href="">Vue全栈课程</a> <span>|</span>
-            <a href="">React全家桶课程</a> <span>|</span>
-            <a href="">微信支付专项课程 (H5+小程序+Node+Mongo)</a>
+            <a href="https://github.com/Benn314/MiMall" target="_blank" >河畔一角主页</a> <span>|</span>
+            <a href="https://github.com/Benn314/MiMall" target="_blank">Vue全栈课程</a> <span>|</span>
+            <a href="https://github.com/Benn314/MiMall" target="_blank">React全家桶课程</a> <span>|</span>
+            <a href="https://github.com/Benn314/MiMall" target="_blank">微信支付专项课程 (H5+小程序+Node+Mongo)</a>
         </div>
         <div class="copyright">
-            Copyright © 2019 mi.51purse.com All Rights Reserved.
+            Copyright © 2019 <span class="domain">mi.51purse.com</span> All Rights Reserved.
         </div>
     </div>
 </template>
@@ -24,11 +24,35 @@ export default {
 
 <style lang="scss">
     .footer{
-        height: 234px;
+        height: 234px; 
         border-top: 4px solid #FF6600;
         background-color: #333333;
         color: #999999;
         font-size: 16px;
         text-align: center;
+        .footer-logo{
+            margin-top:46px;
+            margin-bottom: 31px;
+            img{
+                width:53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color:#999999;
+                display:inline-block;
+            }
+            span{
+                margin:0 10px;
+            }
+            margin-bottom: 13px;
+        }
+        .copyright{
+            .domain{
+                color:#FF6600;
+            }
+        }
     }
 </style>
