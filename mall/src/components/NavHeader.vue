a61fed9a703f55e7f6ec1ef9e89560cb60ee6df9
Benn314 <209441806@qq.com>
2022-09-28 10:18:44 +0800

    service-bar开发完毕

--- mall/src/components/NavHeader.vue
+++ mall/src/components/NavHeader.vue
@@ -165,7 +165,7 @@ export default {
     name: 'nav-header',
     data(){
         return{
-            username:'jack',
+            username:'',
             phoneList:[]
         }
     },
@@ -192,9 +192,10 @@ export default {
                     pageSize:6
                 }
             }).then((res)=>{
-                if(res.list.length>=6){
-                    this.phoneList = res.list.slice(0,6);
-                }
+                // if(res.list.length>=6){
+                    // this.phoneList = res.list.slice(0,6);
+                // }
+                this.phoneList = res.list;
             })
         },
         currency(val) {
@@ -325,7 +326,7 @@ export default {
                             box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
                             z-index: 10;
                             transition: all .5s;
-                            // background-color: #ffffff;
+                            background-color: #ffffff;
                             .product{
                                 position:relative;
                                 float: left;
--- mall/src/pages/home.vue
+++ mall/src/pages/home.vue
@@ -4,7 +4,9 @@
         <!-- nav-header 和 nav-footer 是组件帮助渲染的 -->
         <nav-header></nav-header>   
         <!-- index是 router中redirect帮助重定向的 -->
-        <!-- <router-view></router-view> -->
+        <!-- 没有加router-view 所以挂载到index.vue的内容看不到 需要配置路由呀！ -->
+        <!-- rounter.js中首页的路径重定向为index 所以记得加router-view来连接index.vue -->
+        <router-view></router-view>
         <nav-footer></nav-footer>
     </div>
 </template>
