2a3245eee6ae98ae08d97e727ae2fe35fe43e825
Benn314 <209441806@qq.com>
2022-10-19 22:15:09 +0800

    小米商城完结

--- mall/vue.config.js
+++ mall/vue.config.js
@@ -17,8 +17,9 @@ module.exports = {
   // outputDir:'dist',
   // indexPath:'index2.html',
   // lintOnSave:false, //关闭eslint的校验 但建议开启 它确实能够帮我们做一些本地的代码的语法检查
+  // productionSourceMap在本地的时候可以改成true 线上打包的时候再改成false 否则我们再本地看不到源码也不好
   productionSourceMap:false, //不生成map文件 别人就看不见我们的源码了 开发者工具里【Source】的js文件是被压缩的 别人看不懂
   chainWebpack:(config)=>{
-    config.plugins.delete('prefetch');
+    config.plugins.delete('prefetch'); //通过这种方式把预加载全部干掉 0.js-10.js 真正做到按需加载 防止他重复加载
   }
 }
